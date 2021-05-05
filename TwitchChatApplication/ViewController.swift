33a6d164e1f606827449794cfe94bb19ee9e3248
d1mka112 <d1mka@users.noreply.github.com>
2021-05-05 15:57:31 +0300

    created irc client file

--- TwitchChatApplication/ViewController.swift
+++ TwitchChatApplication/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
